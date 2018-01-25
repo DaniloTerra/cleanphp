11feb0efa0c0113122d4747419a75249e5f4a400
DaniloTerra <danilo_terra@live.com>
2018-01-24 23:45:30 -0200

    Domain Model

---
+++ src/Domain/Entity/Customer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CleanPhp\Invoicer\Domain\Entity;
+
+class Customer extends AbstractEntity
+{
+    private $name;
+    private $email;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+}
